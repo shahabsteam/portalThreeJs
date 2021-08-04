6dda62b975294f1129e52a5a2d8ce29f0da7dfdb
shahabsteam <shahabsteam@gmail.com>
2021-08-04 21:49:32 +0430

    particle animation

--- src/script.js
+++ src/script.js
@@ -89,7 +89,8 @@ const firefliesMaterial = new THREE.ShaderMaterial({
     fragmentShader:firefliesVertexFragmentShader,
     uniforms : {
         uPixelRatio : { value : Math.min(window.devicePixelRatio,2)},
-        uSize : {value : 100}
+        uSize : {value : 100},
+        uTime : {value : 0}
     },
     transparent:true
 },)
@@ -160,6 +161,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    // Update materials
+    firefliesMaterial.uniforms.uTime.value=elapsedTime;
 
     // Update controls
     controls.update()
